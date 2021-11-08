52badbc34de2e9754aff9231490531de94c4b092
Tav Hafner <23143092+haftav@users.noreply.github.com>
2021-11-07 20:25:33 -0700

    Experimenting

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import testSpec from '../testSpec.json';
 import DocsGenerator from './DocsGenerator';
 import Info from './components/Info';
 import Paths from './components/Paths';
+import { OpenApiSchema } from './interfaces';
 
 const App = () => {
   return (
     <div>
-      <DocsGenerator spec={testSpec}>
+      <DocsGenerator spec={testSpec as OpenApiSchema}>
         <Info />
         <Paths />
       </DocsGenerator>
--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -1,4 +1,18 @@
-import { PathObject } from '../interfaces';
+import { PathObject, OperationObject } from '../interfaces';
+
+interface HttpMethodProps {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  endpoint: string;
+  operationObject: OperationObject;
+}
+
+const HttpMethod = ({ method, endpoint, operationObject }: HttpMethodProps) => {
+  return (
+    <div>
+      {method} {endpoint}{' '}
+    </div>
+  );
+};
 
 interface PathProps {
   path: PathObject;
@@ -8,10 +22,19 @@ interface PathProps {
 const Path = ({ path, pattern }: PathProps) => {
   return (
     <div className="border-b border-green-500">
-      <h2>{pattern}</h2>
-      {path.get && <div>get</div>}
-      {path.put && <div>put</div>}
-      {path.post && <div>post</div>}
+      {path.get ? (
+        <div>
+          <HttpMethod method="GET" endpoint={pattern} operationObject={path.get} />
+          <div>{path.get.summary || null}</div>
+          <div>{path.get.description || null}</div>
+        </div>
+      ) : null}
+      {path.post && <HttpMethod method="POST" endpoint={pattern} operationObject={path.post} />}
+      {path.put && <HttpMethod method="PUT" endpoint={pattern} operationObject={path.put} />}
+      {path.delete && (
+        <HttpMethod method="DELETE" endpoint={pattern} operationObject={path.delete} />
+      )}
+      {path.summary && <div>{path.summary}</div>}
     </div>
   );
 };
